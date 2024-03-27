d036618d378ede449cfed79e5cd93805a534bd68
Chamomile Valentin <chamomiv@pr-h5.kzn.21-school.ru>
2024-03-27 17:32:18 +0300

    created database

--- src/database/init_db.sql
+++ src/database/init_db.sql
@@ -1,33 +1,50 @@
-create table if not exist orders
+create table if not exists items
+(
+    chrt_id integer,
+    track_number varchar primary key,
+    price integer,
+    rid varchar,
+    name varchar,
+    sale integer,
+    size varchar,
+    total_price integer,
+    nm_id integer,
+    brand varchar,
+    status integer
+    );
+
+create table if not exists orders
 (
     order_uid varchar primary key,
-    track_number varchar primary key,
-    delivery_name varchar primary key
+    track_number varchar,
     entry varchar,
     locale varchar,
     internal_signature varchar,
     customer_id varchar,
     delivery_service varchar,
     shardkey varchar,
-    sm_id integer varchar,
+    sm_id integer,
     date_created timestamp,
-    oof_shard varchar
+    oof_shard varchar,
+
+    constraint fk_orders_track_number foreign key (track_number) references items (track_number)
 );
 
-create table if not exist delivery
+create table if not exists delivery
 (
+    order_uid varchar,
     name varchar,
     phone varchar,
     zip varchar,
     city varchar,
     address varchar,
     region varchar,
-    email varchar
+    email varchar,
 
-    constraint fk_orders_name foreign key (name) references orders (delivery_name)
+    constraint fk_orders_order_uid foreign key (order_uid) references orders (order_uid)
 );
 
-create table if not exist payment
+create table if not exists payment
 (
     transaction varchar,
     request_id varchar,
@@ -38,24 +55,7 @@ create table if not exist payment
     bank varchar,
     delivery_cost integer,
     goods_total integer,
-    custom_fee integer
+    custom_fee integer,
 
     constraint fk_orders_transaction foreign key (transaction) references orders (order_uid)
-);
-
-create table if not exist items
-(
-    chrt_id integer,
-    track_number varchar,
-    price integer,
-    rid varchar,
-    name varchar,
-    sale integer,
-    size varchar,
-    total_price integer,
-    nm_id integer,
-    brand varchar,
-    status integer
-
-    constraint fk_orders_track_number foreign key (track_number) references orders (track_number)
 );
