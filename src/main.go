301e2474cb82014879fdf509c77183da402f3b0c
Chamomile Valentin <chamomiv@pr-h5.kzn.21-school.ru>
2024-04-02 05:53:36 +0300

    init nats-streaming in project

--- src/main.go
+++ src/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/nats-io/nats.go"
+	"log"
 )
 
 func main() {
-	fmt.Println("aboba")
-	nats_connection, err := nats
+	natsConnection, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Println(err)
+	}
+	err = natsConnection.Publish("aboba", []byte("jotaro kujo"))
+	if err != nil {
+		log.Println(err)
+	}
+	_, err = natsConnection.Subscribe("aboba", func(message *nats.Msg) { fmt.Printf("Received a message: %s\n", string(message.Data)) })
+	if err != nil {
+		log.Println(err)
+	}
+
+	natsConnection.Close()
 }
