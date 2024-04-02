bc0ba9e06ecfac5103ee1e59ea7a7824e8b32b24
Chamomile Valentin <chamomiv@pr-h5.kzn.21-school.ru>
2024-04-02 13:33:18 +0300

    configurated nats-streaming in project

--- src/main.go
+++ src/main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
 	"log"
+	"time"
 )
 
 func main() {
-	natsConnection, err := nats.Connect(nats.DefaultURL)
+	natsStreamConnection, err := stan.Connect("test-cluster", "publisher", stan.NatsURL(stan.DefaultNatsURL))
 	if err != nil {
 		log.Println(err)
 	}
-	err = natsConnection.Publish("aboba", []byte("jotaro kujo"))
+	_, err = natsStreamConnection.Subscribe("aboba", func(message *stan.Msg) { log.Printf("Received a message: %s\n", string(message.Data)) })
 	if err != nil {
 		log.Println(err)
 	}
-	_, err = natsConnection.Subscribe("aboba", func(message *nats.Msg) { fmt.Printf("Received a message: %s\n", string(message.Data)) })
+	err = natsStreamConnection.Publish("aboba", []byte("jotaro kujo"))
 	if err != nil {
 		log.Println(err)
 	}
-
-	natsConnection.Close()
+	time.Sleep(5 * time.Second)
+	err = natsStreamConnection.Publish("aboba", []byte("skibidi"))
+	if err != nil {
+		log.Println(err)
+	}
+	//err = natsStreamConnection.Close()
+	//if err != nil {
+	//	log.Println(err)
+	//}
+	select {}
 }
