9323c4382f8af4e024e278e49b95820fc67f1f3d
Chamomile Valentin <chamomiv@pr-h5.kzn.21-school.ru>
2024-04-07 08:20:31 +0300

    created package readDB for publisher

---
+++ src/database/readDB.go
@@ -0,0 +1,83 @@
+package readDB
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type Orders struct {
+	OrderUid          string     `json:"order_uid"`
+	TrackNumber       string     `json:"track_number"`
+	Entry             string     `json:"entry"`
+	Delivery          []Delivery `json:"delivery"`
+	Payment           []Payment  `json:"payment"`
+	Items             []Items    `json:"items"`
+	Locale            string     `json:"locale"`
+	InternalSignature string     `json:"internal_signature"`
+	CustomerId        string     `json:"customer_id"`
+	DeliveryService   string     `json:"delivery_service"`
+	Shardkey          string     `json:"shardkey"`
+	SmId              int        `json:"sm_id"`
+	DateCreated       time.Time  `json:"date_created"`
+	OofShard          string     `json:"oof_shard"`
+}
+
+type Delivery struct {
+	OrderUid string `json:"order_uid"`
+	Name     string `json:"name"`
+	Phone    string `json:"phone"`
+	Zip      string `json:"zip"`
+	City     string `json:"city"`
+	Address  string `json:"address"`
+	Region   string `json:"region"`
+	Email    string `json:"email"`
+}
+
+type Payment struct {
+	Transaction  string `json:"transaction"`
+	RequestId    string `json:"request_id"`
+	Currency     string `json:"currency"`
+	Provider     string `json:"provider"`
+	Amount       int    `json:"amount"`
+	PaymentDt    int    `json:"payment_dt"`
+	Bank         string `json:"bank"`
+	DeliveryCost int    `json:"delivery_cost"`
+	GoodsTotal   int    `json:"goods_total"`
+	CustomFee    int    `json:"custom_fee"`
+}
+
+type Items struct {
+	ChrtId      int    `json:"chrt_id"`
+	TrackNumber string `json:"track_number"`
+	Price       int    `json:"price"`
+	Rid         string `json:"rid"`
+	Name        string `json:"name"`
+	Sale        int    `json:"sale"`
+	Size        string `json:"size"`
+	TotalPrice  int    `json:"total_price"`
+	NmId        int    `json:"nm_id"`
+	Brand       string `json:"brand"`
+	Status      int    `json:"status"`
+}
+
+func FileOpen(filePath string) ([]byte, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return data, err
+}
+
+func FileDeserialize(fileData []byte) (Orders, error) {
+	var orders Orders
+	err := json.Unmarshal(fileData, &orders)
+	if err != nil {
+		log.Println(err)
+		return orders, err
+	}
+	return orders, nil
+}
--- src/main.go
+++ src/main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
+	readDB "L0/database"
 	"github.com/nats-io/stan.go"
 	"log"
-	"time"
 )
 
 func main() {
+	fileData, _ := readDB.FileOpen("/Users/chamomiv/go/WildBerriesTech-L0/src/model.json")
+
 	natsStreamConnection, err := stan.Connect("test-cluster", "publisher", stan.NatsURL(stan.DefaultNatsURL))
 	if err != nil {
 		log.Println(err)
 	}
-	_, err = natsStreamConnection.Subscribe("aboba", func(message *stan.Msg) { log.Printf("Received a message: %s\n", string(message.Data)) })
+	_, err = natsStreamConnection.Subscribe("jojo", func(message *stan.Msg) { log.Printf("Received a message: %s\n", string(message.Data)) })
 	if err != nil {
 		log.Println(err)
 	}
-	err = natsStreamConnection.Publish("aboba", []byte("jotaro kujo"))
+	err = natsStreamConnection.Publish("jojo", []byte(fileData))
 	if err != nil {
 		log.Println(err)
 	}
-	time.Sleep(5 * time.Second)
-	err = natsStreamConnection.Publish("aboba", []byte("skibidi"))
-	if err != nil {
-		log.Println(err)
-	}
+	//orders, _ := readDB.FileDeserialize(fileData)
 	//err = natsStreamConnection.Close()
 	//if err != nil {
 	//	log.Println(err)
